744fe94986323321d24df2c1b0f5e2ca64634d41
Paloma Rodrigues <palomarodrigues.0013@gmail.com>
2023-08-14 11:56:38 -0300

    typing component SelectCity

---
+++ src/services/getCityByNameService.ts
@@ -0,0 +1,25 @@
+import { api } from './api'
+
+export interface CityProps {
+  id: string
+  name: string
+  latitude: number
+  longitude: number
+}
+
+export async function getCityByNameService(name: string): Promise<CityProps[]> {
+  try {
+    const { data } = await api.get(`/weather?q=${name}`)
+
+    const city = {
+      id: data.id,
+      name: data.sys.country ? `${data.name}, ${data.sys.country}` : data.name,
+      longitude: data.coord.lon,
+      latitude: data.coord.lat,
+    }
+
+    return [city]
+  } catch (error) {
+    return []
+  }
+}
